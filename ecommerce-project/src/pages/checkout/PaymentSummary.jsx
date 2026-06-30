5fecc29e97f73e7ecffd8b7edb91bf4200850471
Jennifer Perez-Juarez <jenniferperez.6190@gmail.com>
2026-06-30 00:05:38 -0400

    Lesson 9 Challenge Exercise: Add test for PaymentSummary that clicks the "Place Order" button

--- ecommerce-project/src/pages/checkout/PaymentSummary.jsx
+++ ecommerce-project/src/pages/checkout/PaymentSummary.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import axios from 'axios';
-import { useNavigate } from 'react-router';
-import { formatMoney } from '../../utils/money';
+import axios from "axios";
+import { useNavigate } from "react-router";
+import { formatMoney } from "../../utils/money";
 
 export function PaymentSummary({ paymentSummary, loadCart }) {
   const navigate = useNavigate();
 
   const createOrder = async () => {
-    await axios.post('/api/orders');
+    await axios.post("/api/orders");
     await loadCart();
-    navigate('/orders');
+    navigate("/orders");
   };
 
   return (
@@ -18,43 +18,61 @@ export function PaymentSummary({ paymentSummary, loadCart }) {
 
       {paymentSummary && (
         <>
-          <div className="payment-summary-row" data-testid="payment-summary-product-cost">
+          <div
+            className="payment-summary-row"
+            data-testid="payment-summary-product-cost"
+          >
             <div>Items ({paymentSummary.totalItems}):</div>
             <div className="payment-summary-money">
               {formatMoney(paymentSummary.productCostCents)}
             </div>
           </div>
 
-          <div className="payment-summary-row" data-testid="payment-summary-shipping-cost">
+          <div
+            className="payment-summary-row"
+            data-testid="payment-summary-shipping-cost"
+          >
             <div>Shipping &amp; handling:</div>
             <div className="payment-summary-money">
               {formatMoney(paymentSummary.shippingCostCents)}
             </div>
           </div>
 
-          <div className="payment-summary-row subtotal-row" data-testid="payment-summary-total-before-tax">
+          <div
+            className="payment-summary-row subtotal-row"
+            data-testid="payment-summary-total-before-tax"
+          >
             <div>Total before tax:</div>
             <div className="payment-summary-money">
               {formatMoney(paymentSummary.totalCostBeforeTaxCents)}
             </div>
           </div>
 
-          <div className="payment-summary-row" data-testid="payment-summary-tax">
+          <div
+            className="payment-summary-row"
+            data-testid="payment-summary-tax"
+          >
             <div>Estimated tax (10%):</div>
             <div className="payment-summary-money">
               {formatMoney(paymentSummary.taxCents)}
             </div>
           </div>
 
-          <div className="payment-summary-row total-row" data-testid="payment-summary-total">
+          <div
+            className="payment-summary-row total-row"
+            data-testid="payment-summary-total"
+          >
             <div>Order total:</div>
             <div className="payment-summary-money">
               {formatMoney(paymentSummary.totalCostCents)}
             </div>
           </div>
 
-          <button className="place-order-button button-primary"
-          onClick={createOrder}>
+          <button
+            className="place-order-button button-primary"
+            data-testid="place-order-button"
+            onClick={createOrder}
+          >
             Place your order
           </button>
         </>
--- ecommerce-project/src/pages/checkout/PaymentSummary.test.jsx
+++ ecommerce-project/src/pages/checkout/PaymentSummary.test.jsx
@@ -1,12 +1,17 @@
 import React from "react";
+import axios from "axios";
 import { it, expect, describe, vi, beforeEach } from "vitest";
 import { render, screen, within } from "@testing-library/react";
-import { MemoryRouter } from "react-router";
+import { MemoryRouter, useLocation } from "react-router";
+import userEvent from "@testing-library/user-event";
 import { PaymentSummary } from "./PaymentSummary";
+
+vi.mock("axios");
 
 describe("PaymentSummary component", () => {
   let loadCart;
   let paymentSummary;
+  let user;
 
   beforeEach(() => {
     paymentSummary = {
@@ -19,6 +24,7 @@ describe("PaymentSummary component", () => {
     };
 
     loadCart = vi.fn();
+    user = userEvent.setup();
   });
 
   it("it displays the correct details", async () => {
@@ -45,5 +51,26 @@ describe("PaymentSummary component", () => {
     expect(screen.getByTestId("payment-summary-total")).toHaveTextContent(
       "$52.51",
     );
+  });
+
+  it("places an order", async () => {
+    function Location() {
+      const location = useLocation();
+      return <div data-testid="url-path">{location.pathname}</div>;
+    }
+
+    render(
+      <MemoryRouter>
+        <PaymentSummary paymentSummary={paymentSummary} loadCart={loadCart} />
+        <Location />
+      </MemoryRouter>,
+    );
+
+    const placeOrderButton = screen.getByTestId("place-order-button");
+    await user.click(placeOrderButton);
+
+    expect(axios.post).toHaveBeenCalledWith("/api/orders");
+    expect(loadCart).toHaveBeenCalled();
+    expect(screen.getByTestId("url-path")).toHaveTextContent("/orders");
   });
 });
