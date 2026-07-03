126a6556284d9b9009e946e01b18843a90eb6fc5
Jennifer Perez-Juarez <jenniferperez.6190@gmail.com>
2026-07-03 15:34:14 -0400

    Lesson 11 Challenge Exercise: Run npm run build in chatbot-project to put this project on the Internet

--- chatbot-project/src/App.tsx
+++ chatbot-project/src/App.tsx
@@ -6,33 +6,37 @@ import robotIcon from "./assets/robot.png";
 import "./App.css";
 
 function App() {
+  const savedMessages = localStorage.getItem("messages");
+
   const [chatMessages, setChatMessages] = useState(
-    JSON.parse(localStorage.getItem("messages")) || [
-      {
-        message: "hello chatbot",
-        sender: "user",
-        id: "id1",
-        time: 1736127288920,
-      },
-      {
-        message: "Hello! How can I help you?",
-        sender: "robot",
-        id: "id2",
-        time: 1736127291230,
-      },
-      {
-        message: "can you get me todays date?",
-        sender: "user",
-        id: "id3",
-        time: 1736127385356,
-      },
-      {
-        message: "Today is September 27",
-        sender: "robot",
-        id: "id4",
-        time: 1736127385500,
-      },
-    ],
+    savedMessages
+      ? JSON.parse(savedMessages)
+      : [
+          {
+            message: "hello chatbot",
+            sender: "user",
+            id: "id1",
+            time: 1736127288920,
+          },
+          {
+            message: "Hello! How can I help you?",
+            sender: "robot",
+            id: "id2",
+            time: 1736127291230,
+          },
+          {
+            message: "can you get me todays date?",
+            sender: "user",
+            id: "id3",
+            time: 1736127385356,
+          },
+          {
+            message: "Today is September 27",
+            sender: "robot",
+            id: "id4",
+            time: 1736127385500,
+          },
+        ],
   );
 
   // const [chatMessages, setChatMessages] = array;
